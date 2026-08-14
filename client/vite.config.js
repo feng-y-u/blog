e41d59dcb81c5ca610abb5c6eb3b70935e869cdb
feng-y-u <1834677757@qq.com>
2026-08-14 14:48:41 +0800

    chore(static): 忽略构建产物，清理vite代理配置

--- client/vite.config.js
+++ client/vite.config.js
@@ -24,9 +24,5 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    proxy: {
-      '/api': 'http://localhost:3001',
-      '/uploads': 'http://localhost:3001',
-    },
   },
 })
